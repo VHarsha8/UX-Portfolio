8a2e80dffb7c813eb08332b5ecaec6b8d50aba0f
VHarsha8 <harshavardhan6304@gmail.com>
2024-10-27 10:41:29 +0530

    Portfolio redesign

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
                 <div className='hidden md:absolute  h-[720px] top-0 -right-[520px]  w-[550px] blur-3xl rounded-full opacity-70 bg-violet-700'></div>
                 <div className='hidden md:absolute h-[400px]  -top-[280px] -right-[100px]  w-[350px] blur-xl rounded-full opacity-65  bg-pink-700'></div>
                 <div className='flex relative   justify-start  w-full md:w-[800px] mx-auto  h-screen md:px-0 px-4 py-16  flex-col gap-8'>
-                <div class="md:block hidden  absolute inset-0 h-full w-full bg-transparent bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]"></div>                    <div className='flex flex-col gap-2'>
+                <div class="block  absolute inset-0 h-full w-full bg-transparent bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]"></div>                    <div className='flex flex-col gap-2'>
                          <div className='z-50 flex' >
                             <img src={MyImage} alt="pr" className='w-10 rounded-full' />
                         </div>
