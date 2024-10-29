52f7c27d72843bd5d8eab320c7d2ba1a5495af66
Harshavardhan <140392137+VHarsha8@users.noreply.github.com>
2024-10-29 11:44:36 +0530

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -8,7 +8,8 @@ const Projects = () =>{
             <img src= {RailNav} alt="mr" className='w-3/4 md:w-1/2'/>
             </div>
             <div className='flex flex-col flex-wrap gap-6 p-6'>
-                     <h1 className='text-5xl text-white font-ibm-plex-mono '>Aha <span className='text-md text-gray-300 font-ibm-plex-mono'>a Restaurant app</span>.</h1>
+                     <h1 className='text-5xl text-white font-ibm-plex-mono '>Aha.</h1>
+                     <h1 className='text-xl text-white font-ibm-plex-mono '>a Restaurant app</h1>
                      <p className='text-white text-wrap font-space-mono'>I'm driven by curiosity, mixing analog and digital media to develop novel visual expressions for wallpapers and my work on AI based systems.</p>
                      <ButtonComponent text="Visit" link="https://www.figma.com/proto/fwXxkHlf41V4Yer2amnh8O/Rael?node-id=37-208&node-type=canvas&t=DkkqgXmDfVFIQJ2w-0&scaling=scale-down&content-scaling=fixed&page-id=0%3A1" />
 
