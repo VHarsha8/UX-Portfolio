fb7432666728310cda555128ac8f19d035e2ea3f
VHarsha8 <harshavardhan6304@gmail.com>
2024-10-31 11:40:26 +0530

    Portfolio redesign

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,7 +3,7 @@ import ButtonComponent from '../components/ButtonComponent';
 
 const Projects = () =>{
     return(
-        <div className='md:w-[800px] md:p-0 overflow-hidden  relative p-6 mt-6 bg-black border border-white/15 rounded-2xl mx-4 md:mx-auto md:flex-row-reverse flex flex-col  '>
+        <div className='md:w-[800px] md:p-0 overflow-hidden  relative p-6 mt-6 bg-black border border-white/30 rounded-2xl mx-4 md:mx-auto md:flex-row-reverse flex flex-col  '>
                         <div className='absolute  w-full h-full rounded-full bg-white/20 blur-3xl md:bottom-[40%] md:right-[60%] bottom-[60%] right-[40%]'></div>
 
             <div className='flex items-center justify-center '>
--- src/pages/Projects1.jsx
+++ src/pages/Projects1.jsx
@@ -2,7 +2,7 @@ import ButtonComponent from '../components/ButtonComponent';
 
 const Projects1 = () =>{
     return(
-        <div className='md:w-[800px] md:p-0 overflow-hidden  relative p-6 mt-6 bg-black border border-white/15 rounded-2xl mx-4 md:mx-auto md:flex-row-reverse flex flex-col  '>
+        <div className='md:w-[800px] md:p-0 overflow-hidden  relative p-6 mt-6 bg-black border border-white/30 rounded-2xl mx-4 md:mx-auto md:flex-row-reverse flex flex-col  '>
                         <div className='absolute  w-full h-full rounded-full bg-white/20 blur-3xl md:bottom-[40%] md:right-[60%] bottom-[60%] right-[40%]'></div>
             <div className='flex items-center justify-center '>
             <img src="/ahabhojanammockup2.png" alt="mr" className='z-30 w-3/4 md:w-fit '/>
