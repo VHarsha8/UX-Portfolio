a356e1d2f89912342ffc38590c741c1362f89d4a
VHarsha8 <harshavardhan6304@gmail.com>
2024-10-31 10:50:15 +0530

    Portfolio redesign

--- src/pages/Projects1.jsx
+++ src/pages/Projects1.jsx
@@ -8,7 +8,7 @@ const Projects1 = () =>{
             <img src="/ahabhojanammockup2.png" alt="mr" className='w-fit'/>
             </div>
             <div className='flex flex-col flex-wrap gap-6 p-0 md:p-6 md:justify-center'>
-                            <h1 className='text-4xl text-white md:text-5xl font-ibm-plex-mono '>Aha Bhojanam.</h1>
+                            <h1 className='z-30 text-4xl text-white md:text-5xl font-ibm-plex-mono '>Aha Bhojanam.</h1>
                      
                      <p className='hidden text-white md:flex text-wrap font-space-mono'>Designed an intuitive restaurant app interface that streamlines menu browsing, ordering, and payment, enhancing the dining experience with a visually appealing and user-friendly layout tailored for convenience.</p>
                      <ButtonComponent text="Visit" link="https://www.figma.com/proto/qXllmLJBVyQuZ1sgbhWvch/Design-2?node-id=0-1&node-type=canvas&t=rasO6HAMX5LptxS5-0&scaling=scale-down&content-scaling=fixed&page-id=0%3A1" className='z-30' />
