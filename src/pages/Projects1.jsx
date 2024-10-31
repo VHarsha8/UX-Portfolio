735ed02b75f0d37e2c69b7b5d72b3309c3dcf57f
VHarsha8 <harshavardhan6304@gmail.com>
2024-10-31 10:49:09 +0530

    Portfolio redesign

--- src/pages/Projects1.jsx
+++ src/pages/Projects1.jsx
@@ -2,8 +2,8 @@ import ButtonComponent from '../components/ButtonComponent';
 
 const Projects1 = () =>{
     return(
-        <div className='md:w-[800px] md:px-0  overflow-hidden relative p-6 mt-6 bg-blue-700/5 border border-white/30 rounded-2xl mx-4 md:mx-auto md:flex-row-reverse flex flex-col  '>
-            <div className='absolute w-full h-full rounded-full bg-blue-700/40 blur-3xl top-[30%] right-[40%]'></div>
+        <div className='md:w-[800px] md:px-0  overflow-hidden relative p-6 mt-6 bg-blue-700/10 border border-white/30 rounded-2xl mx-4 md:mx-auto md:flex-row-reverse flex flex-col  '>
+            <div className='absolute w-full h-full rounded-full bg-blue-700/30 blur-3xl top-[30%] right-[40%]'></div>
             <div className='flex items-center justify-center '>
             <img src="/ahabhojanammockup2.png" alt="mr" className='w-fit'/>
             </div>
